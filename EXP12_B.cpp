2aa6bd6716456b8851ebc330d697abce72067e80
sarakanyal03 <sarakanyal@gmail.com>
2024-08-29 22:22:06 +0530

    Add files via upload

---
+++ EXP12_B.cpp
@@ -0,0 +1,27 @@
+// NAME - SARA KANYAL
+// PRN - 23070123115
+// EXPERIMENT 12 B
+
+#include<iostream>
+using namespace std;
+class Num
+{
+    public:
+    Num(int c, int d)
+    {
+        if(c>d)
+        {
+            cout<<c<<" is greater.";
+        }
+        else 
+        {
+            cout<<d<<" is greater.";
+        }
+    }
+};
+int main()
+{
+    Num n1(4,3);
+} 
+
+
---
+++ EXP12_C.cpp
@@ -0,0 +1,29 @@
+// NAME - SARA KANYAL
+// PRN - 23070123115
+// EXPERIMENT 12C 
+
+#include<iostream>
+using namespace std;
+int count=0;
+class destruct{
+    public:
+    destruct()
+    {
+        count++;
+        cout<<"Number of objects created: "<<count<<"\n";
+    }
+    ~destruct()
+    {
+        count--;
+        cout<<"Number of objects destroyed: "<<count<<"\n";
+    }
+};
+int main()
+{
+    destruct aa,bb,cc;
+    {
+        destruct dd;
+    }
+    return 0;
+} 
+
